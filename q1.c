b04b9c5794c1e7ba550d2ba26fca9c21d215d541
BrandonPilane <BrandonPilane@gmail.com>
2025-09-01 09:19:59 +0200

    First question

---
+++ q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  // Get user's name
+  char name[20];
+  printf("Enter your name: ");
+  scanf("%19s", name);
+
+  // Get user's age
+  int age;
+  printf("Enter your age: ");
+  scanf("%d", &age);
+
+  // Print out user's information
+  printf("You are %s, you are %d years old\n", name, age);
+
+  return 0;
+}
